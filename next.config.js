ac959118dce09d65235a6ad769db7a2442b44524
sonil <sonilcerneaux@gmail.com>
2025-12-22 16:53:23 +0100

    fix: configure @svgr/webpack for turbopack instead of webpack

--- next.config.js
+++ next.config.js
@@ -1,27 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg')
-    )
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
-      {
-        test: /\.svg$/i,
-        resourceQuery: { not: /url/ },
-        use: ['@svgr/webpack'],
-      }
-    )
-
-    fileLoaderRule.exclude = /\.svg$/i
-
-    return config
+    },
   },
   images: {
     remotePatterns: [
