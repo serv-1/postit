01217715d6830fe78c47be0c55d9a7c47c82e628
sonil <sonilcerneaux@gmail.com>
2025-12-22 17:02:35 +0100

    fix: TypeScript errors in Select component

--- components/Select/index.tsx
+++ components/Select/index.tsx
@@ -1,14 +1,8 @@
-import ReactSelect, {
-  type StylesConfig,
-  type MultiValue,
-  type CSSObjectWithLabel,
-} from 'react-select'
+import ReactSelect, { type CSSObjectWithLabel } from 'react-select'
 import {
   useController,
   type FieldValues,
   type FieldPath,
-  type PathValue,
-  type Path,
 } from 'react-hook-form'
 import type { StateManagerProps } from 'react-select/dist/declarations/src/useStateManager'
 import type { Categories } from 'types'
@@ -32,53 +26,6 @@ export default function Select<FormFields extends FieldValues = FieldValues>({
   const { field, formState } = useController<FormFields>({ name })
   const { isSubmitted, errors } = formState
 
-  const styles: StylesConfig<Option> = {
-    control: (provided, states) => {
-      const styles: CSSObjectWithLabel = {
-        display: 'flex',
-        height: 40,
-        backgroundColor: '#FDF4FF',
-        borderRadius: '4px',
-      }
-
-      if (isSubmitted && errors[name]) {
-        styles.border = `2px solid #${states.isFocused ? '881337' : 'E11D48'}`
-      } else {
-        const a = states.isFocused ? '0.75' : '0.25'
-        styles.borderBottom = `2px solid rgba(112,26,117,${a})`
-      }
-
-      return styles
-    },
-    menu: (provided) => ({
-      ...provided,
-      backgroundColor: '#fdf4ff',
-      borderBottom: '2px solid rgba(112,26,117,0.25)',
-      zIndex: 9999,
-    }),
-    placeholder: (provided) => ({ ...provided, color: 'rgba(112,26,117,0.5)' }),
-    option: (provided, state) => {
-      return {
-        ...provided,
-        backgroundColor: state.isFocused ? '#f5d0fe' : provided.backgroundColor,
-        '&:hover': { backgroundColor: '#f5d0fe' },
-      }
-    },
-    indicatorSeparator: () => ({ display: 'none' }),
-    valueContainer: (provided) => ({ ...provided, overflowY: 'auto' }),
-  }
-
-  const value = options.filter(({ value }) => {
-    if (Array.isArray(field.value)) {
-      return field.value.includes(value)
-    }
-  })
-
-  const onChange = (val: MultiValue<Option>) =>
-    field.onChange(
-      val.map(({ value }) => value) as PathValue<FormFields, Path<FormFields>>
-    )
-
   return (
     <ReactSelect
       isMulti
@@ -86,9 +33,60 @@ export default function Select<FormFields extends FieldValues = FieldValues>({
       instanceId={field.name}
       inputId={field.name}
       options={options}
-      styles={styles}
-      value={value}
-      onChange={onChange}
+      styles={{
+        control: (provided, states) => {
+          const styles: CSSObjectWithLabel = {
+            display: 'flex',
+            height: 40,
+            backgroundColor: '#FDF4FF',
+            borderRadius: '4px',
+          }
+
+          if (isSubmitted && errors[name]) {
+            styles.border =
+              '2px solid #' + (states.isFocused ? '881337' : 'E11D48')
+          } else {
+            styles.borderBottom = `2px solid rgba(112,26,117,${
+              states.isFocused ? '0.75' : '0.25'
+            })`
+          }
+
+          return styles
+        },
+        menu: (provided) => {
+          provided.backgroundColor = '#fdf4ff'
+          provided.borderBottom = '2px solid rgba(112,26,117,0.25)'
+          provided.zIndex = 9999
+
+          return provided
+        },
+        placeholder: (provided) => {
+          provided.color = 'rgba(112,26,117,0.5)'
+
+          return provided
+        },
+        option: (provided, state) => {
+          if (state.isFocused) {
+            provided.backgroundColor = '#f5d0fe'
+          }
+
+          provided['&:hover'] = { backgroundColor: '#f5d0fe' }
+
+          return provided
+        },
+        indicatorSeparator: () => ({ display: 'none' }),
+        valueContainer: (provided) => {
+          provided.overflowY = 'auto'
+
+          return provided
+        },
+      }}
+      value={options.filter(({ value }) => {
+        if (Array.isArray(field.value)) {
+          return field.value.includes(value)
+        }
+      })}
+      onChange={(val) => field.onChange(val.map(({ value }) => value))}
       {...props}
     />
   )
