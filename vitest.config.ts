cfef9498fe299cbfc266254cd4ad0a81bd696223
sonil <sonilcerneaux@gmail.com>
2026-05-29 18:06:59 +0200

    fix: vitest warning about plugin vite-tsconfig-paths being useless
    - uninstall vite-tsconfig-paths
    - replace the plugin by `resolve.tsconfigPaths: true` in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { loadEnvConfig } from '@next/env'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     globals: true,
     env: loadEnvConfig(process.cwd()).parsedEnv,
